2a0104b335e0f8072682340bb106b6d60c875d57
eze.delrio.dev@gmail.com <eze.delrio.dev@gmail.com>
2024-03-26 13:41:49 +0100

    feat: improve design podcast-card

--- src/components/podcast-card/podcast-card.tsx
+++ src/components/podcast-card/podcast-card.tsx
@@ -1,15 +1,22 @@
+import { FC } from "react";
+import { Entry } from "../../types/types";
 import "./podcast-card.style.css";
-const PodcastCard = () => {
-  const image =
-    "https://is1-ssl.mzstatic.com/image/thumb/Podcasts113/v4/f2/21/fa/f221fabd-017f-5125-633b-f1fe4f39802a/mza_182995249085044287.jpg/170x170bb.png";
+
+type Props = {
+  podcastItem: Entry;
+};
+
+const PodcastCard: FC<Props> = (props) => {
+  const { podcastItem } = props;
+
   return (
     <div className="card__container">
       <div className="card__rectangle">
-        <div className="card__title">ALL SONG CONSIDERER</div>
-        <div className="card__author">Author: Song Explorer</div>
+        <div className="card__title">{podcastItem["im:name"].label}</div>
+        <div className="card__author">Author: {podcastItem["im:artist"].label}</div>
       </div>
       <div className="card__circle">
-        <img src={image} alt="Images" className="card__image" />
+        <img src={podcastItem["im:image"][2].label} alt="Images" className="card__image" />
       </div>
     </div>
   );
--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,9 +1,15 @@
 import PodcastCard from "../../components/podcast-card/podcast-card";
+import mock from "../../mocks/podcast-list.json";
+import { Entry } from "../../types/types";
+import "./home.style.css";
 
 const Home = () => {
+  const podcastList: Entry[] = mock.feed.entry;
   return (
-    <div data-testid="home_container">
-      <PodcastCard />
+    <div className="home__container" data-testid="home_container">
+      {podcastList.map((item) => (
+        <PodcastCard podcastItem={item} key={item.id.label} />
+      ))}
     </div>
   );
 };
