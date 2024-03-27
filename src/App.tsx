5afe85f497e6bceb67b292151edc835f199ef758
eze.delrio.dev@gmail.com <eze.delrio.dev@gmail.com>
2024-03-27 15:45:03 +0100

    feat: add indes routes files

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Header from "./components/header/header";
-import Home from "./screens/home/home";
-import PodcastDetail from "./screens/podcast-detail/podcast-detail";
-import EpisodeDetail from "./screens/episode-detail/episode-detail";
+import PodcastRoutes from "./router";
 
 function App() {
   return (
-    <Router>
+    <>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/podcast/:podcastId" element={<PodcastDetail />} />
-        <Route path="/podcast/:podcastId/episode/:episodeId" element={<EpisodeDetail />} />
-
-        <Route path="/*" element={<Navigate to="/" />} />
-      </Routes>
-    </Router>
+      <PodcastRoutes />)
+    </>
   );
 }
 export default App;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,20 +1,16 @@
-import { BrowserRouter as Router, Route, Navigate, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "../screens/home/home";
-import Header from "../components/header/header";
 import PodcastDetail from "../screens/podcast-detail/podcast-detail";
 import EpisodeDetail from "../screens/episode-detail/episode-detail";
 
 function PodcastRoutes() {
   return (
     <Routes>
-      <Router>
-        <Header />
-        <Route path="/" element={<Home />} />
-        <Route path="/podcast/:podcastId" element={<PodcastDetail />} />
-        <Route path="/podcast/:podcastId/episode/:episodeId" element={<EpisodeDetail />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/podcast/:podcastId" element={<PodcastDetail />} />
+      <Route path="/podcast/:podcastId/episode/:episodeId" element={<EpisodeDetail />} />
 
-        <Route path="/*" element={<Navigate to="/" />} />
-      </Router>
+      <Route path="/*" element={<Navigate to="/" />} />
     </Routes>
   );
 }
