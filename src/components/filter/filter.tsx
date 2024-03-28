a93862feb5336cbef367c6edbad60eacaa89791c
eze.delrio.dev@gmail.com <eze.delrio.dev@gmail.com>
2024-03-28 10:44:49 +0100

    feat: add logic filter

--- src/components/filter/filter.tsx
+++ src/components/filter/filter.tsx
@@ -1,10 +1,34 @@
+import { FC, ChangeEvent, useState } from "react";
 import "./filter.style.css";
 
-const Filter = () => {
+type Props = {
+  handleFilterChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  listLength: number;
+};
+
+const Filter: FC<Props> = (props) => {
+  const { handleFilterChange, listLength } = props;
+  const [focused, setFocused] = useState<boolean>(false);
+
+  const handleFocus = () => {
+    setFocused(true);
+  };
+
+  const handleBlur = () => {
+    setFocused(false);
+  };
+
   return (
     <div className="filter__container">
-      <span className="filter__counter">999</span>
-      <input className="filter__input" type="text" placeholder="Filter podcast..." />
+      <span className="filter__counter">{listLength}</span>
+      <input
+        className="filter__input"
+        type="text"
+        onChange={handleFilterChange}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        placeholder={focused ? "" : "Filter podcast..."}
+      />
     </div>
   );
 };
--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,17 +1,45 @@
+import { ChangeEvent, useEffect, useState } from "react";
 import Filter from "../../components/filter/filter";
 import PodcastCard from "../../components/podcast-card/podcast-card";
 import { usePodcastListConnect } from "../../hooks/connect/usePodcastListConnect";
 import "./home.style.css";
+import { Entry } from "../../types/types";
 
 const Home = () => {
   const podcastListQuery = usePodcastListConnect();
+  const [filteredPodcastList, setFilteredPodcastList] = useState<Entry[]>([]);
+  const [podcastList, setPodcastList] = useState<Entry[]>([]);
 
-  const podcastList = podcastListQuery.data?.feed.entry;
+  useEffect(() => {
+    console.log({ filteredPodcastList });
+  }, [filteredPodcastList]);
+
+  useEffect(() => {
+    if (podcastListQuery.data) {
+      setFilteredPodcastList(podcastListQuery.data.feed.entry);
+      setPodcastList(podcastListQuery.data.feed.entry);
+    }
+  }, [podcastListQuery.data]);
+
+  const filterPodcastList = (filterBy: string) => {
+    const filtered = podcastList.filter(
+      (podcast) =>
+        podcast["im:name"].label.toLowerCase().includes(filterBy) ||
+        podcast["im:artist"].label.toLowerCase().includes(filterBy),
+    );
+    if (podcastListQuery.data) setFilteredPodcastList(filtered);
+  };
+
+  const handleFilterChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const filterBy = event.target.value.toLowerCase();
+    filterPodcastList(filterBy);
+  };
+
   return (
     <div className="home__container">
-      <Filter />
+      <Filter handleFilterChange={handleFilterChange} listLength={filteredPodcastList.length} />
       <div className="home__podcastList" data-testid="home_container">
-        {podcastList?.map((item) => (
+        {filteredPodcastList?.map((item) => (
           <PodcastCard podcastItem={item} key={item.id.label} />
         ))}
       </div>
