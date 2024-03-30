5c317a15f9232b998d978d4015963c98a408a8fc
eze.delrio.dev@gmail.com <eze.delrio.dev@gmail.com>
2024-03-30 22:30:50 +0100

    feat: removed link from podcast detail

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import "./sidebar.style.css";
 import { useQueryClient } from "@tanstack/react-query";
 import { Podcast } from "../../types/listTypes";
+import { useLocation } from "react-router-dom";
 
 interface Props {
   id: number;
@@ -16,6 +17,12 @@ const Sidebar: FC<Props> = (props) => {
   const queryClient = useQueryClient();
   const queryKey = ["podcastList"];
   const data = queryClient.getQueryData<Podcast>(queryKey);
+  const { pathname } = useLocation();
+
+  const isEpisodeScreen = pathname
+    .split("/")
+    .filter((element) => element !== "")
+    .includes("episode");
 
   const searchDescription = () => {
     return data?.feed.entry.find((element) => element.id.attributes["im:id"] === id.toString())?.summary.label;
@@ -23,18 +30,25 @@ const Sidebar: FC<Props> = (props) => {
 
   return (
     <aside className="sidebar__container">
-      <Link to={`/podcast/${id}`} className="sidebar__image">
+      <Link to={`/podcast/${id}`} className={`sidebar__image ${isEpisodeScreen ? "" : "link-no-cursor"}`}>
         <img className="podcast-detail__artwork" src={image} alt={title} />
       </Link>
 
       <hr className="sidebar__line" />
-
       <div className="sidebar__info">
-        <Link to={`/podcast/${id}`} className="sidebar__title" style={{ textDecoration: "none", color: "inherit" }}>
+        <Link
+          to={`/podcast/${id}`}
+          className={`sidebar__title ${isEpisodeScreen ? "" : "link-no-cursor"}`}
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
           {title}
         </Link>
 
-        <Link to={`/podcast/${id}`} className="sidebar__author" style={{ textDecoration: "none" }}>
+        <Link
+          to={`/podcast/${id}`}
+          className={`sidebar__author ${isEpisodeScreen ? "" : "link-no-cursor"}`}
+          style={{ textDecoration: "none" }}
+        >
           By: {artistName}
         </Link>
 
