e9a8c6b8ef90b5e5adc93a8f54b15bb760b2701f
eze.delrio.dev@gmail.com <eze.delrio.dev@gmail.com>
2024-03-31 12:58:44 +0200

    feat: add control loading podcastListQuery

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
 import "./header.style.css";
 import LoadingIcon from "../loading-icon/loading-icon";
+import { usePodcastListConnect } from "../../hooks/connect/usePodcastListConnect";
 
 const Header = () => {
+  const podcastListQuery = usePodcastListConnect();
+
   return (
     <header className="header__container">
       <Link to="/" className="header__link">
         Podcaster
       </Link>
-      <LoadingIcon />
+      {podcastListQuery.isLoading && <LoadingIcon />}
     </header>
   );
 };
