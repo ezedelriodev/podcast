dd63144d6f21f23d44f2b29d34e3862cdc25f8e5
eze.delrio.dev@gmail.com <eze.delrio.dev@gmail.com>
2024-03-31 19:46:40 +0200

    fix: fix problem re render sidebar

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,18 +2,16 @@ import { Link } from "react-router-dom";
 import "./header.style.css";
 import LoadingIcon from "../loading-icon/loading-icon";
 import { usePodcastListConnect } from "../../hooks/connect/usePodcastListConnect";
-import { usePodcastDetailConnect } from "../../hooks/connect/usePodcastDetailConnect";
 
 const Header = () => {
   const podcastListQuery = usePodcastListConnect();
-  const podcastDetailQuery = usePodcastDetailConnect();
 
   return (
     <header className="header__container">
       <Link to="/" className="header__link">
         Podcaster
       </Link>
-      {(podcastListQuery.isLoading || podcastDetailQuery.isFetching) && <LoadingIcon />}
+      {podcastListQuery.isLoading && <LoadingIcon />}
     </header>
   );
 };
